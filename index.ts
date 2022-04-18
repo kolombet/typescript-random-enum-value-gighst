0eeb091cdda827b452e5a5ef989bca465740cc97
kolombet <anonrab@ya.ru>
2022-04-18 19:51:10 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,17 +1,36 @@
-//fifo - first in - first out
-class Stack<T> {
-  array = [];
-  push(val:T) {
-    this.array.push(val);
+//Stack = lifo - last in - first out
+//Queue = fifo - first in - first out
+
+class Queue<T> {
+  #array = [];
+  push(val: T) {
+    this.#array.push(val);
   }
 
-  pop():T {
-    return this.array.pop();
-  };
+  pop(): T {
+    return this.#array.shift();
+  }
 }
-const queue = new Stack<string>();
+const queue = new Queue<string>();
 queue.push('message1');
 queue.push('message2');
+queue.push('message3');
+queue.push('message4');
+queue.push('message5');
+//queue.#array = [];
 
-queue.pop(); //message1
-queue.pop(); //message2
+// let el = null;
+// while(el = queue.pop()) {
+//   console.log(el);
+// }
+
+function sendMessage() {
+  const message = queue.pop();
+  if (!message) return;
+  //emulate server request
+  setTimeout(() => {
+    console.log('message sent: ' + message);
+    sendMessage();
+  }, 1000);
+}
+sendMessage();
